ae833cf8b5db66fca2720f2c3b37a3026649f3dd
Yanghb1202 <439378479@qq.com>
2026-07-11 22:51:40 +0800

    init: 初始化冰冰记账本小程序项目

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 App({
   onLaunch() {
-    // 展示本地存储能力
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        env: 'cloud1-d7g8unf00d7aa7b00',
+        traceUser: true
+      })
+    }
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
