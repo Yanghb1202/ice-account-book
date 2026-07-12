908a129f9bd2a310fca53c3bb58f74d6b42968b7
Yanghb1202 <439378479@qq.com>
2026-07-12 22:29:05 +0800

    feat: 完成云开发环境配置，上传云函数

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        env: 'cloud1-xxxxx',
+        env: 'cloud1-d7g8unf00d7aa7b00',
         traceUser: true
       })
     }
